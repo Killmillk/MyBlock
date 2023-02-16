d2f80675e7569407ef7758719385a9b3347625d2
arman <110356055+Killmillk@users.noreply.github.com>
2023-02-16 21:17:59 +0300

    Add files via upload

---
+++ Blockchain.cpp
@@ -0,0 +1,23 @@
+//
+// Created by S.A.I
+//
+
+#include "Blockchain.h"
+
+Blockchain::Blockchain()
+{
+    _vChain.emplace_back(Block(0, "Genesis Block"));
+    _nDifficulty = 10; //сложность 
+}
+
+void Blockchain::AddBlock(Block bNew)
+{
+    bNew.sPrevHash = _GetLastBlock().sHash;
+    bNew.MineBlock(_nDifficulty);
+    _vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const
+{
+    return _vChain.back();
+}
